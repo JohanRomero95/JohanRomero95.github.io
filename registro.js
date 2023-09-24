2eceebe5831598f6a63cb9659cf9278a1095be02
JohanRomero95 <johanarm95@gmail.com>
2023-09-24 19:25:13 -0300

    arreglando detalles

--- registro.js
+++ registro.js
@@ -3,55 +3,59 @@ const nombreRegex = /^[a-zA-Z ]{2,30}$/;
 const correoRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const contrasenaRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
 
-const openModal = document.getElementById("registro");
-const modal1 = document.querySelector(".modal");
-const modalBackground = document.querySelector(".modal-background");
+const registro = document.getElementById("registro");
+const modal = document.querySelector(".modal");
+const botonCerrarModal = document.querySelector(".modal__close");
 
-document
-     .getElementById("registroForm")
-     .addEventListener("submit", function (e) {
-          e.preventDefault();
+// Función para mostrar el modal
+function mostrarModal() {
+     modal.classList.add("modal--show");
 
-          // Obtener valores de los campos del formulario
-          const nombre = document.getElementById("nombre").value;
-          const correo = document.getElementById("correo").value;
-          const contrasena = document.getElementById("contrasena").value;
-
-          // Validar los campos utilizando expresiones regulares
-          if (!nombreRegex.test(nombre)) {
-               return;
-          }
-
-          if (!correoRegex.test(correo)) {
-               return;
-          }
-
-          if (!contrasenaRegex.test(contrasena)) {
-               return;
-          }
-
-          // Almacenar los datos en el almacenamiento local
-          const usuario = {
-               nombre,
-               correo,
-               contrasena,
-          };
-          localStorage.setItem("usuario", JSON.stringify(usuario));
-
-          // Función modal
-          const openModalClick = (e) => {
-               e.preventDefault();
-               modal1.classList.add("modal--show");
-
-               // Agregar un setTimeout para retrasar la redirección en 10 segundos
-               setTimeout(() => {
-                    window.location.href = "login.html";
-               }, 10000);
-          };
-
-          // Agregar un evento de clic al botón de registro
-          openModal.addEventListener("click", openModalClick);
-
-          // Redireccionar a la página de inicio de sesión
+     setTimeout(() => {
           window.location.href = "login.html";
-     });
+     }, 10000);
+}
+
+// Funcion cerrar modal
+botonCerrarModal.addEventListener("click", (e) => {
+     e.preventDefault();
+     //  modal.classList.remove("modal--show");
+     window.location.href = "login.html";
+});
+
+// Agregar un evento de clic al botón de registro
+registro.addEventListener("click", function (e) {
+     e.preventDefault();
+
+     // Obtener valores de los campos del formulario
+     const nombre = document.getElementById("nombre").value;
+     const correo = document.getElementById("correo").value;
+     const contrasena = document.getElementById("contrasena").value;
+
+     // Validar los campos utilizando expresiones regulares
+     if (!nombreRegex.test(nombre)) {
+          // alert("Nombre no válido");
+          return;
+     }
+
+     if (!correoRegex.test(correo)) {
+          // alert("Correo electrónico inválido");
+          return;
+     }
+
+     if (!contrasenaRegex.test(contrasena)) {
+          // alert("La contraseña debe tener al menos 6 caracteres, una mayúscula, una minúscula y un número.");
+          return;
+     }
+
+     // Almacenar los datos en el almacenamiento local
+     const usuario = {
+          nombre,
+          correo,
+          contrasena,
+     };
+     localStorage.setItem("usuario", JSON.stringify(usuario));
+
+     // Mostrar el modal una vez que se han validado los datos y se ha guardado en localStorage
+     mostrarModal();
+});
